92d2bf9dc44534b3eeac3c2e7613e485868ad0c5
Nopseudo <nopseudonline@gmail.com>
2018-01-08 19:16:25 +0100

    V0.0

---
+++ Exo1/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<!le truc qui fait que l'asccii est nickel>
+<html>
+<head>
+<TITLE>
+
+</TITLE>
+</head>
+<body>
+<!---HEAD-->
+<div id="head">
+
+
+
+</div>
+
+<!-- Le menu -->
+<div id="menu">
+    <div class="element_menu">
+        <h3>Titre menu</h3>
+        <ul>
+        <li><a href="Exo1P1.html">Lien</a></li>
+        <li><a href="Exo1P2.html">Lien</a></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Le corps -->
+<div id="corps">
+<h1>HEADER</h1>
+<p>
+TITRE<br />
+HELLO WORLD
+</p>
+</div>
+<!-- Le pied de page -->
+<div id="pied_de_page">
+<p>Copyright NOPSEUDONLINE</p>
+</div>
+</body>
+
+</html>
